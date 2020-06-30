9d88265e0ad764710aa1d76bab062960c8b96acb
marcelovasconcellos <marcelomdevasconcellos@gmail.com>
2020-06-29 21:26:20 -0300

    initial

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Install-Pentaho-Data-Integration-PDI-Ubuntu-18.04
-Install Pentaho Data Integration (PDI) Ubuntu 18.04
+# README - Install Pentaho Data Integration Ubuntu 18.04 #
+
+This README document steps necessary to get your application up and running.
+
+
+### How to install? ###
+
+1. Open Ubuntu 18.04 (LTS) x64 Terminal or access your cloud with ssh, using the command below:
+
+	`ssh <user>@<ip_number>`
+
+2. Execute the command to clone this repository:
+
+	`sudo apt install git`
+
+	`git clone https://github.com/marcelomdevasconcellos/Install-Pentaho-Data-Integration-PDI-Ubuntu-18.04.git`
+
+3. Execute the command:
+
+	`cd Install-Pentaho-Data-Integration-PDI-Ubuntu-18.04`
+	
+	`source install_pdi_unbuntu.sh`
+
+
+### What will be installed? ###
+
+* Java 8.0.252
+* Pentaho Data Integration 9.0.0.0-423
---
+++ install_pdi_unbuntu.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+select yn in Yes No
+do
+    case $yn in
+        Yes ) 
+            echo "Installing Java 8.0.252.hs-adpt ..."
+            # https://dev.to/awwsmm/installing-and-running-hadoop-and-spark-on-ubuntu-18-393h
+
+            java -version
+            sudo apt autoremove
+            sudo apt install curl -y
+            sudo apt install unzip
+            sudo apt install zip
+            curl -s "https://get.sdkman.io" | bash
+            echo "source ~/.sdkman/bin/sdkman-init.sh" >> ~/.bashrc
+            source ~/.bashrc
+            sdk install java 8.0.252.hs-adpt
+            echo "export JAVA_HOME=~/.sdkman/candidates/java/8.0.252.hs-adpt/" >> ~/.bashrc
+            break;;
+        No ) 
+            break;;
+    esac
+done
+
+sudo apt-get install libwebkitgtk-1.0-0
+echo ""
+echo "Downloading Pentaho Data Integration 9.0.0.0-423 ..."
+echo ""
+wget https://ufpr.dl.sourceforge.net/project/pentaho/Pentaho%209.0/client-tools/pdi-ce-9.0.0.0-423.zip
+unzip pdi-ce-9.0.0.0-423.zip -d ./pentaho
+
+echo ""
+echo ""
+echo "For execute PDI (Pentaho Data Integration) execute:"
+echo ""
+echo "$ cd pentaho/data-integration/"
+echo "$ ./spoon.sh"
+echo ""
+echo ""
